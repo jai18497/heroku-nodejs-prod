286bf0ef49ac46f868cb248b0cf47c92d95bbea9
jai18497 <78148881+jai18497@users.noreply.github.com>
2021-01-28 20:44:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Heroku Test - AIT!')
 })
 
 app.listen(app.get('port'), function() {
